abb6fb96c196a8fed59ece4d3a4c1488de8bdd52
percymcn <benzemm110@gmail.com>
2026-01-20 01:52:51 -0500

    feat(05-09): export MT4Adapter from adapters package
    - Added MT4Adapter import and export to __init__.py
    - MT4Adapter now accessible from app.infrastructure.adapters

--- app/infrastructure/adapters/__init__.py
+++ app/infrastructure/adapters/__init__.py
@@ -18,10 +18,13 @@ Each adapter:
 # Broker adapter exports
 # Will be populated as each broker adapter is implemented
 
+from app.infrastructure.adapters.mt4_adapter import MT4Adapter
+from app.infrastructure.adapters.mt5_adapter import MT5Adapter
+
 __all__ = [
     # "TradeLockerAdapter",  # Plan 05-06
     # "TopStepAdapter",       # Plan 05-07
     # "TradovateAdapter",     # Plan 05-08
-    # "MT4Adapter",           # Plan 05-09
-    # "MT5Adapter",           # Plan 05-10
+    "MT4Adapter",           # Plan 05-09
+    "MT5Adapter",           # Plan 05-10
 ]
